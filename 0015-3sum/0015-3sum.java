3d08300b7a981028cff26edd1763b020c21ff543
Kamalkant RajpuT <kamalkant5555k@gmail.com>
2026-09-17 10:46:20 +0530

    Time: 533 ms (10.87%), Space: 60.7 MB (5.07%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -31,9 +31,6 @@ class Solution {
         int n=nums.length;
         Arrays.sort(nums);
         for(int i=0;i<n;i++){
-            // if(i > 0 && nums[i] == nums[i - 1]) {
-            //     continue;
-            // }
             int l=i+1;
             int h=n-1;
             while(l<h){
@@ -50,12 +47,6 @@ class Solution {
                    
                     l++;
                     h--;
-                    // while(l<h && nums[l]==nums[l-1]){
-                    //     l++;
-                    // }
-                    // while(l<h && nums[h]==nums[h+1]){
-                    //     h--;
-                    // }
                 }else if(sum<0){
                     l++;
                 }else{
